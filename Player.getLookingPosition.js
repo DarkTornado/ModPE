12cdfe4c757ef9a4babd8df9f966854a44f8f61d
Dark Tornado <dt3141592@naver.com>
2021-02-10 01:51:33 +0900

    바라보고 있는 곳의 좌표 불러오기

---
+++ Player.getLookingPosition.js
@@ -0,0 +1,36 @@
+/*
+ * int[] Player.getLookingPosition(int limit[, int[] blockIds]);
+ * Get Position where player is looking.
+ * limit: Distance limit.
+ * blockIds: Blocks to be treated as empty spaces. Air if omitted.
+ */
+Player.getLookingPosition = function(limit, blockIds) {
+    if (blockIds == undefined) blockIds = [0];
+    var px, py, pz, yaw, sin, cos, tan, pcos;
+    px = Player.getX();
+    py = Player.getY();
+    pz = Player.getZ();
+    yaw = Math.floor(getYaw());
+    pitch = Math.floor(getPitch());
+    sin = -Math.sin(yaw / 180 * Math.PI);
+    cos = Math.cos(yaw / 180 * Math.PI);
+    tan = -Math.sin(pitch / 180 * Math.PI);
+    pcos = Math.cos(pitch / 180 * Math.PI);
+    for (var n = 0; n < limit; n++) {
+        if (blockIds.indexOf(getTile(px + n * sin * pcos, py + n * tan, pz + n * cos * pcos)) == -1) {
+            return [px + n * sin * pcos, py + n * tan, pz + n * cos * pcos];
+        }
+    }
+    return null;
+}
+
+
+/* Example */
+function useItem(x, y, z, i, b) {
+    if (i == 280) {
+        var pos = Player.getLookingPosition(50);
+        if (pos == null) clientMessage("Destination is too far.");
+        else Entity.setPosition(Player.getEntity(), pos[0], pos[1] + 1, pos[2]);
+    }
+}
+
